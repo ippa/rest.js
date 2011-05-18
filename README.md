cf07517e636745c919ae492b36f24fed308d7d1d
ippa <iplord@iplord.com>
2011-05-18 06:27:36 +0200

    added JSONP support (on by default), login/password, spruce up the example

--- README.md
+++ README.md
@@ -3,18 +3,19 @@ rest.js
 * Interact with restfull APIs through JavaScript
 * No dependencies
 
-Usage
+Syntax
 -----
 <pre>
   restjs.get(url, callback)
   restjs.post(url, data, callback)
   restjs.put(url, data, callback)
   restjs.delete(url, callback)
-
   restjs.unpack()           /* make get, post, put & del (delete is a reserved word) available as top level methods */
   restjs.login(login)       /* set login to use in all requests */
   restjs.password(password) /* set password to use in all requests */
   restjs.baseURL(url)       /* prefix all requests with this url */
+  restjs.useJSONP()         /* use JSONP for all requests */
+  restjs.useJSON()          /* use normal JSON for all requests */
 </pre>
 
 happy apiing,
--- rest.js
+++ rest.js
@@ -8,7 +8,8 @@
 
 (function(restjs) {  
   var base_url, login, password
-  
+  var use_jsonp = true
+
   function createCallback(callback) {
     return function onreadystatechange(e) {
       if (this.readyState != 4) { return }
@@ -16,19 +17,51 @@
     }
   }
 
-  restjs.login = function(l)                  { login = l; return restjs; }
-  restjs.password = function(p)               { password = p; return restjs; }
-  restjs.baseURL = function(url)              { base_url = url; return restjs; }
+  function createID() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for(var i=0; i < 10; i++) text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text
+  }
+
+  restjs.useJSON = function()                 { use_jsonp = false; return restjs }
+  restjs.useJSONP = function()                { use_jsonp = true; return restjs }
+  restjs.login = function(l)                  { login = l; return restjs }
+  restjs.password = function(p)               { password = p; return restjs }
+  restjs.baseURL = function(url)              { base_url = url; return restjs }
   restjs.get = function(url, callback)        { restjs.request('GET', url, null, callback)  }
   restjs.post = function(url, data, callback) { restjs.request('POST', url, data, callback) }
   restjs.put = function(url, data, callback)  { restjs.request('PUT', url, data, callback)  }
   restjs.delete = function(url, callback)     { restjs.request('DELETE', url, callback)     }
  
   restjs.request = function(method, url, data, callback) {
-    var req = new XMLHttpRequest()
-    req.onreadystatechange = createCallback(callback)
-    req.open(method, url, true, login, password)
-    data ? req.send(data) :  req.send(null)
+    if(use_jsonp) {
+      var script = document.createElement("script")
+      var callback_name = createID()
+      window[callback_name] = function(data) { callback(data) }
+  
+      url += (url.indexOf("?")==-1) ? "?" : "&"   /* Add ? or & depending on how the current URL looks */
+      url += "callback=" + callback_name
+
+      if(method != "GET") url += "?_method=" + method;
+      script.type = "text/javascript" 
+      script.src = url
+      document.getElementsByTagName('head')[0].appendChild(script)
+      console.log(url)
+    }
+    else {
+      var req = new XMLHttpRequest()
+      req.onreadystatechange = createCallback(callback)
+      req.open(method, url, true, login, password)
+      data ? req.send(data) :  req.send(null)
+    }
+  }
+  
+  restjs.unpack = function() {
+    window.get = restjs.get
+    window.post = restjs.post
+    window.put = restjs.put
+    window.del = restjs.delete
   }
 
   window.restjs = restjs
