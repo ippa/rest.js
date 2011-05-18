47c433433ac56ac927d8de8aa464e2eb2aeed23f
ippa <iplord@iplord.com>
2011-05-18 19:01:41 +0200

    make callback function names a bit longer

--- rest.js
+++ rest.js
@@ -20,7 +20,7 @@
   function createID() {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for(var i=0; i < 10; i++) text += possible.charAt(Math.floor(Math.random() * possible.length));
+    for(var i=0; i < 20; i++) text += possible.charAt(Math.floor(Math.random() * possible.length));
     return text
   }
 
